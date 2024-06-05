5a7cdcd3551c7cbd3d1d115a7514b472ddf979fa
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-06-05 08:53:19 +0530

    Update Sort by Absolute Difference.cpp

--- Sorting/Sort by Absolute Difference.cpp
+++ Sorting/Sort by Absolute Difference.cpp
@@ -1,1 +1,9 @@
-//will be updated soon
+class Solution{
+    public:
+    void sortABS(int A[], int N, int k)
+    {
+        stable_sort(A, A + N, [k](int a, int b) {
+            return abs(a - k) < abs(b - k);
+        });
+    }
+};
