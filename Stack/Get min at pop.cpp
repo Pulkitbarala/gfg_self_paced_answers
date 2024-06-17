9ef72cb42389ea8471c415a7fb0dc7a97734675b
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-06-17 11:06:16 +0530

    Update Get min at pop.cpp

--- Stack/Get min at pop.cpp
+++ Stack/Get min at pop.cpp
@@ -1,1 +1,24 @@
-//will be updated
+stack<int> minStack;
+stack<int> _push(int arr[],int n)
+{
+    stack<int> s;
+    for (int i = 0; i < n; ++i) {
+        s.push(arr[i]);
+        
+        if (minStack.empty()) {
+            minStack.push(arr[i]);
+        } else {
+            minStack.push(min(minStack.top(), arr[i]));
+        }
+    }
+    
+    return s;
+}
+void _getMinAtPop(stack<int>s)
+{
+    while (!s.empty()) {
+        s.pop();
+        cout << minStack.top() << " ";
+        minStack.pop();
+    }
+}
