056ac5dc5b5bc64eb9f53f37fc52454c5b8b4988
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-06-17 11:30:59 +0530

    Update Fraction Trouble.cpp

---
+++ Array/Fraction Trouble.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> LargestFraction(int n, int d) {
+        int maxP = 0, maxQ = 1;
+
+        for (int q = d; q > 0 && q >= d - 10000; --q) {
+            int p = (n * q - 1) / d;
+            if (p >= 1 && __gcd(p, q) == 1) {
+                if (1LL * maxP * q < 1LL * p * maxQ) {
+                    maxP = p;
+                    maxQ = q;
+                }
+            }
+        }
+        
+        for (int q = d + 1; q <= 10000; ++q) {
+            int p = (n * q - 1) / d;
+            if (p >= 1 && __gcd(p, q) == 1) {
+                if (1LL * maxP * q < 1LL * p * maxQ) {
+                    maxP = p;
+                    maxQ = q;
+                }
+            }
+        }
+        
+        return {maxP, maxQ};
+    }
+};
