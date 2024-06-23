d7653f172194ec28025f2bccbfc3d0cff283bb65
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-06-23 18:49:25 +0530

    Update Betting game.cpp

--- Array/Betting game.cpp
+++ Array/Betting game.cpp
@@ -1,1 +1,30 @@
-//will be updated soon
+class Solution{
+    public:
+    int betBalance(string result)
+    {
+        
+        int N, i, balance = 4, bet = 1;
+        N = result.size();
+        
+        //loop for calculating the final balance
+        for(i = 0;i < N;i++)
+        {
+            if(result[i] == 'W')
+            {
+                balance = balance + bet;
+                bet = 1;
+            }
+            else
+            {
+                balance = balance - bet;
+                bet = bet * 2;
+            }
+            if(balance < bet && i < N-1)
+            {
+                balance = -1;
+                break;
+            }
+        }
+        return balance;
+    }
+};
