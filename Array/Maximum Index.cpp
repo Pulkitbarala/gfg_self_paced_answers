357e94f4321cb6664f2bff504c1f6f5a5963f038
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-07-20 21:30:52 +0530

    Update Maximum Index.cpp

--- Array/Maximum Index.cpp
+++ Array/Maximum Index.cpp
@@ -25,3 +25,39 @@ public:
     }
 
 };
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+class Solution {
+  public:
+    int maxIndexDiff(vector<int>& arr) {
+        int n = arr.size();
+        if (n <= 1) return 0;
+
+        vector<int> leftMin(n);
+        vector<int> rightMax(n);
+
+        // Fill leftMin[] such that leftMin[i] contains the minimum value from arr[0] to arr[i]
+        leftMin[0] = arr[0];
+        for (int i = 1; i < n; ++i) {
+            leftMin[i] = min(arr[i], leftMin[i-1]);
+        }
+
+        // Fill rightMax[] such that rightMax[j] contains the maximum value from arr[j] to arr[n-1]
+        rightMax[n-1] = arr[n-1];
+        for (int j = n-2; j >= 0; --j) {
+            rightMax[j] = max(arr[j], rightMax[j+1]);
+        }
+
+        // Traverse leftMin[] and rightMax[] to find the maximum j - i
+        int i = 0, j = 0, maxDiff = 0;
+        while (i < n && j < n) {
+            if (leftMin[i] <= rightMax[j]) {
+                maxDiff = max(maxDiff, j - i);
+                ++j;
+            } else {
+                ++i;
+            }
+        }
+
+        return maxDiff;
+    }
+};
