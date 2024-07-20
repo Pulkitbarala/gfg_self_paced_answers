8b89d9787c17b94105909b076227910366554aef
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-07-20 21:47:45 +0530

    Update Roof Top.cpp

--- Searching/Roof Top.cpp
+++ Searching/Roof Top.cpp
@@ -19,3 +19,23 @@ class Solution
 }
 
 };
+-------------------------------------------------------------------------
+class Solution {
+  public:
+    int maxStep(vector<int> arr) {
+        int max_steps = 0;
+        int current_steps = 0;
+        
+        for (int i = 0; i < arr.size() - 1; ++i) {
+            if (arr[i] < arr[i + 1]) {
+                current_steps++;
+            } else {
+                max_steps = max(max_steps, current_steps);
+                current_steps = 0;
+            }
+        }
+        
+        max_steps = max(max_steps, current_steps);
+        return max_steps;
+    }
+};
