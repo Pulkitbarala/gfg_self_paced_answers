3ec6c50fed66b1ba8b40393ed9d4a792fd1a4a81
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-07-20 21:33:50 +0530

    Update Reverse array in groups.cpp

--- Array/Reverse array in groups.cpp
+++ Array/Reverse array in groups.cpp
@@ -14,3 +14,19 @@ public:
         }
     }
 };
+---------------------------------------------------------------------------------------
+class Solution {
+  public:
+    void reverseInGroups(vector<long long int> &arr, int k) {
+        int n = arr.size();
+        for (int i = 0; i < n; i += k) {
+            int left = i;
+            int right = min(i + k - 1, n - 1);
+            while (left < right) {
+                swap(arr[left], arr[right]);
+                left++;
+                right--;
+            }
+        }
+    }
+};
