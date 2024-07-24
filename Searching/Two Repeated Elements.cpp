bf68fb86ad06780d56bc4023c341b5889bb0dd69
Pulkit <121921642+Pulkitbarala@users.noreply.github.com>
2024-07-24 06:33:05 +0530

    Update Two Repeated Elements.cpp

--- Searching/Two Repeated Elements.cpp
+++ Searching/Two Repeated Elements.cpp
@@ -1,3 +1,24 @@
+//this is updated code
+class Solution {
+  public:
+    vector<int> twoRepeated(int n, int arr[]) {
+        vector<int> result;
+        
+        for (int i = 0; i < n + 2; i++) {
+            int index = abs(arr[i]) - 1;
+            if (arr[index] < 0) {
+                result.push_back(abs(arr[i]));
+                if (result.size() == 2) break;
+            } else {
+                arr[index] = -arr[index];
+            }
+        }
+        
+        return result;
+    }
+};
+-----------------------------------------------------------------------------------------------------------------------------------------
+//this is previous code
 class Solution {
 public:
     vector<int> twoRepeated(int arr[], int n) {
